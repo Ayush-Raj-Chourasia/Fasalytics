ba865c4dccd435aac0a1602f8fc13f4877c4921b
Ayush Raj Chourasia <iter.student.alpha@gmail.com>
2026-02-13 22:38:27 +0530

    fix: Update Django settings module path in manage.py and settings.py

--- manage.py
+++ manage.py
@@ -1,7 +1,7 @@
 import os, sys
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'agri_platform_backend.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
     from django.core.management import execute_from_command_line
     execute_from_command_line(sys.argv)
 
